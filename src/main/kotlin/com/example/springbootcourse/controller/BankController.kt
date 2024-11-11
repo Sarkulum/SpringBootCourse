2d0fe346d19bb5a4cb860cd6705fe6f5fe682eaa
Sarkulum <derWolf100@gmx.de>
2024-11-11 19:10:45 +0100

    some changes

--- src/main/kotlin/com/example/springbootcourse/controller/BankController.kt
+++ src/main/kotlin/com/example/springbootcourse/controller/BankController.kt
@@ -1,4 +1,7 @@
 package com.example.springbootcourse.controller
 
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class BankController {
 }
--- src/test/kotlin/com/example/springbootcourse/controller/BankControllerTest.kt
+++ src/test/kotlin/com/example/springbootcourse/controller/BankControllerTest.kt
@@ -1,5 +1,24 @@
 package com.example.springbootcourse.controller
 
 import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import kotlin.test.Test
 
-class BankControllerTest
+@SpringBootTest
+@AutoConfigureMockMvc
+class BankControllerTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `should return all banks`(){
+        //when
+
+        //then
+
+    }
+}
